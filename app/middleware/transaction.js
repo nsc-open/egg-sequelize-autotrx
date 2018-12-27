6045097329de963ae2e8b94cb97c5b139707732c
Admin <ygjack414@hotmail.com>
2018-12-27 15:32:39 +0800

    update

--- app/middleware/transaction.js
+++ app/middleware/transaction.js
@@ -1,4 +1,41 @@
 module.exports = (options, app) => async (ctx, next) => {
+
   console.log('my plugin', options)
+  console.log('app.config', app.config)
+  
+  const { namespace } = app.config.sequelizeAutoTrx
+  const sequelizeConfig = app.config.sequelize
+
+  if (sequelizeConfig.datasources) { // multiple datasources
+    sequelizeConfig.datasources.forEach(datasource => {
+      inject(ctx, getDelegate(datasource), namespace)  
+    })
+  } else { // single datasource
+    inject(ctx, getDelegate(sequelizeConfig), namespace)
+  }
+
   await next()
+}
+
+// refer: https://github.com/eggjs/egg-sequelize#multiple-datasources
+const getDelegate = datasource => datasource.delegate || 'model'
+
+const inject = (ctx, delegate, namespace) => {
+  const model = ctx[delegate]
+
+  if (!model.transaction.__injected) {
+    const oldTrx = model.transaction
+    model.transaction = async task => {
+      let transaction = namespace.get('transaction')
+      if (transaction) {
+        app.loggers.coreLogger.info(`[egg-sequelize-autotrx] ctx.[${delegate}].transaction call injected transaction method`)
+        return oldTrx.call(model, { transaction }, task)
+      } else {
+        app.loggers.coreLogger.info(`[egg-sequelize-autotrx] ctx.[${delegate}].transaction call original transaction method`)
+        return oldTrx.call(model, task)
+      }
+    }
+    model.transaction.__injected = true
+    app.loggers.coreLogger.info(`[egg-sequelize-autotrx] ctx.[${delegate}].transaction is injected`)
+  }
 }
