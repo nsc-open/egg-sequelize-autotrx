43b76cdb8736400fa662dfc5695b1707a3b4f9fe
Admin <ygjack414@hotmail.com>
2018-12-27 14:57:20 +0800

    update

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const assert = require('assert')
 
 module.exports = app => {
-  const index = app.config.coreMiddleware.indexOf('sequelize')
+  console.log('egg-sequelize-autotrx app.js')
+  /*const index = app.config.coreMiddleware.indexOf('sequelize')
 
   // egg-sequelize is required
   assert(index >= 0, 'sequelize plugin is required')
 
   // put this plugin after egg-sequelize
-  app.config.coreMiddleware.splice(index + 1, 0, 'sequelizeAutotrx')
+  app.config.coreMiddleware.splice(index + 1, 0, 'sequelizeAutotrx') */
+  app.config.coreMiddleware.push('transaction')
 }
